1724bf26624f8b1d2c805aa7d6eeca674f5f987d
Anuj Singh <fitanuj@gmail.com>
2024-06-10 21:03:18 +0530

    c++ learning

---
+++ Course-2-C++/C++ Programs/five.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,x;
+    cout<<"Enter an integer";
+    cin>>a;
+    x=a>>1;
+    cout<<"\nvalue of a is "<<a;
+    cout<<"\nvalue of x is "<<x;
+    return 0;
+}
---
+++ Course-2-C++/C++ Programs/six.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,s;
+    cout<<"Enter first Integer ";
+    cin>>a;
+    cout<<"Enter second Integer ";
+    cin>>b;
+    cout<<"Enter third Integer ";
+    cin>>c;
+    s=a+b+c;
+    cout<<"sum is "<<s;
+    return 0;
+
+}
