674ef4bd4b625744b8b3bcb8eaf781d4359947eb
Anuj Singh <fitanuj@gmail.com>
2024-06-10 14:40:12 +0530

    c++ learning

---
+++ Course-2-C++/C++ Programs/three.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char x='A';
+    cout<<(int)x;
+    return 0;
+}
---
+++ Course-2-C++/C++ Programs/two.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My Second C++ Program";
+    return 0;
+}
