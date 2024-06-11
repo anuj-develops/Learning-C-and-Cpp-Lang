5802b8c6d22b722449767bc474ef0c99da075446
Anuj Singh <fitanuj@gmail.com>
2024-06-11 23:39:44 +0530

    c++ learning

---
+++ Course-2-C++/C++ Programs/seven.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,s;
+    cout<<"Enter three integers ";
+    cin>>a>>b>>c;
+    s=a+b+c;
+    cout<<"sum is "<<s;
+    return 0;
+}
